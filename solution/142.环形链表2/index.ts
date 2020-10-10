d7e41e93a3c790cf4cc47a77e770839aadd8f160
StuMisaka <husteiczhu@163.com>
2020-10-10 11:36:25 +0800

    :memo: 更新环形链表2 哈希表解法

---
+++ solution/142.环形链表2/index.ts
@@ -0,0 +1,56 @@
+// 给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+
+// 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+
+// 说明：不允许修改给定的链表。
+
+//  
+
+// 示例 1：
+
+// 输入：head = [3,2,0,-4], pos = 1
+// 输出：tail connects to node index 1
+// 解释：链表中有一个环，其尾部连接到第二个节点。
+
+
+// 示例 2：
+
+// 输入：head = [1,2], pos = 0
+// 输出：tail connects to node index 0
+// 解释：链表中有一个环，其尾部连接到第一个节点。
+
+
+// 示例 3：
+
+// 输入：head = [1], pos = -1
+// 输出：no cycle
+// 解释：链表中没有环。
+
+
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/linked-list-cycle-ii
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function detectCycle(head: ListNode | null): ListNode | null {
+  let map = new Set();
+  let node: ListNode | null = head;
+  while(node){
+    if(map.has(node)){
+      return node
+    }
+    map.add(node);
+    node = node.next;
+  }
+  return null
+};

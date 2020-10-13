c8ef18da9e4a7976aed6d633b93076ce621580bf
StuMisaka <husteiczhu@163.com>
2020-10-13 14:22:41 +0800

    :memo: 更新21.合并两个有序链表 递归解法

--- solution/21.合并两个有序链表/index.ts
+++ solution/21.合并两个有序链表/index.ts
@@ -24,7 +24,7 @@
 
  //循环遍历
 function mergeTwoLists(l1: ListNode | null, l2: ListNode | null): ListNode | null {
-  let ans = new ListNode()
+  let ans = new ListNode();
   let pre = ans;
   while(l1 && l2){
     if(l1.val < l2.val){
@@ -40,3 +40,21 @@ function mergeTwoLists(l1: ListNode | null, l2: ListNode | null): ListNode | nul
 
   return ans.next
 };
+
+//递归
+function mergeTwoLists(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+  if(!l1){
+    return l2
+  }
+  if(!l2){
+    return l1
+  }
+  if(l1.val < l2.val){
+    l1.next = mergeTwoLists(l1.next,l2);
+    return l1
+  }
+  else{
+    l2.next = mergeTwoLists(l1,l2.next);
+    return l2
+  }
+}

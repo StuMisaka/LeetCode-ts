3d11cf245693f4de4f8efcd2230ab0d0509f9996
StuMisaka <husteiczhu@163.com>
2020-10-15 09:53:12 +0800

    :memo: 更新88.合并两个有序数组 从后向前遍历解法

--- solution/88.合并两个有序数组/index.ts
+++ solution/88.合并两个有序数组/index.ts
@@ -61,7 +61,22 @@ function merge(nums1: number[], m: number, nums2: number[], n: number): void {
   }
 };
 
-merge([1,2,3,0,0,0]
-  ,3
-  ,[2,5,6]
-  ,3)
+//从后往前遍历
+function merge(nums1: number[], m: number, nums2: number[], n: number): void{
+  let i: number = m - 1;
+  let j: number = n - 1;
+  let k: number = m + n - 1;
+  while(i >= 0 && j >= 0){
+    if(nums1[i] >= nums2[j]){
+      nums1[k] = nums1[i];
+      k--;
+      i--;
+    }
+    else{
+      nums1[k] = nums2[j];
+      k--;
+      j--;
+    }
+  }
+  nums1.splice(0,j+1,...nums2.slice(0,j+1));
+}
